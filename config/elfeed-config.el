e3c2d1e940e4c80c35555161b55b84d6676019ad
Karl Hallsby <karl@hallsby.com>
2026-06-15 12:54:30 -0500

    Add Adrian Sampson's blog to elfeed

--- config/elfeed-config.el
+++ config/elfeed-config.el
@@ -63,6 +63,7 @@
                   ("https://justine.lol/rss.xml" systems compilers blog)
                   ("https://yazandaba.hashnode.dev/rss.xml" compilers blog)
                   ("https://cfallin.org/feed.xml" compilers blog)
+                  ("https://www.cs.cornell.edu/~asampson/blog.xml" languages compilers hardware blog)
                   ("https://atillahallsby.com/feed/" blog rhetoric research)))
   ;; NOTE: Make elfeed use Emacs' built-in url-retrieve function rather than cURL.
   ;; This is significantly faster, but only works on Emacsen running on Linux
